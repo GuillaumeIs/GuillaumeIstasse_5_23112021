3285bdbef150942c8c80e5fada2c0ed02afbfd68
GuillaumeIs <you@example.com>
2022-01-22 16:02:56 +0100

    maj cart.js - add formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -15,6 +15,7 @@ function kanapArticle() {
         for (let i = 0; i < localStorage.length; i++) {
             //Récupération de la fonction kanapLocal stocké dans la variable kanap
             let kanap = kanapLocal(i);
+            //Liaison de l'élement cart__items et affichage de chaque nouveau élement
             cartItems.innerHTML +=
                 `<article class="cart__item" data-id="${kanap.id}" data-color="${kanap.couleur}" data-price="${kanap.prix}">
                 <div class="cart__item__img">
@@ -38,7 +39,61 @@ function kanapArticle() {
                 </div>
                 </article>`;
         }} else {
+        //Annonce du pannier vide stocké dans la variable pVide
         let pVide = `<p>Votre panier est vide.</p>`;
+        //Liaison du panier vide et affichage du message
         cartItems.innerHTML = pVide;
 }};
 kanapArticle();
+
+function nouveauFormulaire() {
+    //Récupération de la valeur de l'élement firstName stocké dans la varible utiPrenom
+    let utiPrenom = document.querySelector("#firstName").value;
+    //Récupération de la valeur de l'élement lastName stocké dans la varible utiNom
+    let utiNom = document.querySelector("#lastName").value;
+    //Récupération de la valeur de l'élement address stocké dans la varible utiAdresse
+    let utiAdresse = document.querySelector("#address").value;
+    //Récupération de la valeur de l'élement city stocké dans la varible utiVille
+    let utiVille = document.querySelector("#city").value;
+    //Récupération de la valeur de l'élement email stocké dans la varible utiEmail
+    let utiEmail = document.querySelector("#email").value;
+
+    //Regex Nom
+    let regexNom = /^[a-zA-Z]/;
+    //Regex Prénom
+    let regexPrenom = /^[a-zA-Z]/;
+    //Regex Adresse
+    let regexAdress = /^[a-zA-Z0-9-]/;
+    //Regex Ville
+    let regexCity = /^[a-zA-Z]/;
+    //Regex Email
+    let regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]/;
+
+    //Récupération de l'élement firstNameErrorMsg stocké dans la varible firstNameErrorMsg
+    let firstNameErrorMsg = document.querySelector("#firstNameErrorMsg");
+    //Récupération de l'élement lastNameErrorMsg stocké dans la varible lastNameErrorMsg
+    let lastNameErrorMsg = document.querySelector("#lastNameErrorMsg");
+    //Récupération de l'élement addressErrorMsg stocké dans la varible addressErrorMsg
+    let addressErrorMsg = document.querySelector("#addressErrorMsg");
+    //Récupération de l'élement cityErrorMsg stocké dans la varible cityErrorMsg
+    let cityErrorMsg = document.querySelector("#cityErrorMsg");
+    //Récupération de l'élement emailErrorMsg stocké dans la varible emailErrorMsg
+    let emailErrorMsg = document.querySelector("#emailErrorMsg");
+
+    if (regexNom.test(utiPrenom)) {
+    firstNameErrorMsg.innerHTML = "";
+    if (regexPrenom.test(utiNom)) {
+    lastNameErrorMsg.innerHTML = "";
+    if (regexAdress.test(utiAdresse)) {
+    addressErrorMsg.innerHTML = "";
+    if (regexCity.test(utiVille)) {
+    cityErrorMsg.innerHTML = "";
+    if (regexEmail.test(utiEmail)) {
+    emailErrorMsg.innerHTML = "";
+    return true;
+    } else { emailErrorMsg.innerHTML = "Veuillez entrer une adresse email valide."; }
+    } else { cityErrorMsg.innerHTML = "Veuillez entrer un nom de ville."; }
+    } else { addressErrorMsg.innerHTML = "Veuillez entrer une adresse postale."; }
+    } else { lastNameErrorMsg.innerHTML = "Veuillez entrer un nom."; }
+    } else { firstNameErrorMsg.innerHTML = "Veuillez entrer un prénom."; }
+};
